45c1c0e748c0f3afa08cc707aad0907259920e54
adel radwan <adelbios11@gmail.com>
2018-06-25 10:07:39 +0300

    Initial Commit

---
+++ hn/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  hn
+//
+//  Created by adel radwan on 11/10/1439 AH.
+//  Copyright © 1439 adel radwan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
